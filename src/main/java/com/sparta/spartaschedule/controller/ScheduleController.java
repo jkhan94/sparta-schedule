424f0dafe8489e56dde874b88a548ba06796c62c
jkhan94 <hjkwork@naver.com>
2024-05-15 05:17:14 +0900

    Swagger 추가, index.html 배경 범위 수정

--- src/main/java/com/sparta/spartaschedule/controller/ScheduleController.java
+++ src/main/java/com/sparta/spartaschedule/controller/ScheduleController.java
@@ -3,6 +3,10 @@ package com.sparta.spartaschedule.controller;
 import com.sparta.spartaschedule.dto.ScheduleRequestDto;
 import com.sparta.spartaschedule.dto.ScheduleResponseDto;
 import com.sparta.spartaschedule.entity.Schedule;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -12,8 +16,10 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.*;
 import java.util.List;
 
+// Swagger 링크: http://localhost:8080/swagger-ui/index.html#/
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Schedule CRUD", description = "스케줄 등록, 조회, 수정, 삭제 컨트롤러")
 public class ScheduleController {
     private final JdbcTemplate jdbcTemplate;
 
@@ -22,6 +28,15 @@ public class ScheduleController {
     }
 
     @PostMapping("/schedules")
+    @Operation(summary = "스케줄 등록", description = "제목, 내용, 담당자, 비밀번호, 작성일을 전달받아 스케줄을 디비에 저장할 때 사용하는 API")
+    @Parameters({
+            @Parameter(name="id", description = "스케줄 고유번호",example = "5"),
+            @Parameter(name="title", description = "할 일 제목",example = "스프링 입문 과제"),
+            @Parameter(name="contents", description = "할 일 내용",example = "요구사항에 맞춰서 스케줄 CRUD 구현"),
+            @Parameter(name="responsibility", description = "담당자",example = "수강생A"),
+            @Parameter(name="password", description = "스케줄 수정 또는 삭제 시 필요한 비밀번호",example = "540"),
+            @Parameter(name="createdDate", description = "스케줄 작성일",example = "2024-05-15"),
+    })
     public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto requestDto) {
         Schedule schedule = new Schedule(requestDto);
         KeyHolder keyHolder = new GeneratedKeyHolder();
@@ -30,7 +45,6 @@ public class ScheduleController {
         jdbcTemplate.update(con -> {
                     PreparedStatement preparedStatement = con.prepareStatement(sql,
                             Statement.RETURN_GENERATED_KEYS);
-
                     preparedStatement.setString(1, schedule.getTitle());
                     preparedStatement.setString(2, schedule.getContents());
                     preparedStatement.setString(3, schedule.getResponsibility());
@@ -51,6 +65,7 @@ public class ScheduleController {
     }
 
     @GetMapping("/schedules")
+    @Operation(summary = "스케줄 조회", description = "디비에 저장된 스케줄의 제목, 내용, 담당자, 작성일을 조회할 때 사용하는 API")
     public List<ScheduleResponseDto> getSchedules() {
         String sql = "SELECT * FROM schedule ORDER BY createdDate desc";
 
@@ -69,6 +84,7 @@ public class ScheduleController {
     }
 
     @PutMapping("/schedules/{id}/{password}")
+    @Operation(summary = "스케줄 수정", description = "입력받은 비밀번호와 디비의 비밀번호가 일치할 경우 전달받은 스케줄 아이디의 일정을 수정.")
     public ScheduleResponseDto updateSchedule(@PathVariable Long id, @PathVariable String password, @RequestBody ScheduleRequestDto requestDto) {
         Schedule schedule = findById(id);
         if (schedule != null && schedule.getPassword().equals(password)) {
@@ -84,6 +100,7 @@ public class ScheduleController {
     }
 
     @DeleteMapping("/schedules/{id}/{password}")
+    @Operation(summary = "스케줄 삭제", description = "입력받은 비밀번호와 디비의 비밀번호가 일치할 경우 전달받은 스케줄 아이디의 일정을 삭제")
     public void deleteSchedule(@PathVariable Long id, @PathVariable String password) {
         // 해당 메모가 DB에 존재하는지 확인
         Schedule schedule = findById(id);
